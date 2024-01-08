2019a2085a9c30fbb4f49e9bd49d86559233d221
NataliiaKulesh <ivankulesh0200@gmail.com>
2024-01-08 23:09:33 +0200

    class Bank

--- src/main/java/org/example/Bank.java
+++ src/main/java/org/example/Bank.java
@@ -1,5 +1,46 @@
 package org.example;
 
+import java.util.List;
+import java.util.ArrayList;
+
+
+//Реалізуйте клас Bank,
+//який зберігає колекцію об'єктів BankAccount
+
+
 public class Bank {
     private List<BankAccount> accounts = new ArrayList<>();
+//створити акаунт
+public BankAccount createAccount(String accountName, double initialDeposit) {
+    int accountNumber = generateAccountNumber();
+    BankAccount account = new BankAccount(accountNumber, accountName, initialDeposit);
+    accounts.add(account);
+    return account;
+}
+
+//пошук
+    public BankAccount findAccount(int accountNumber) throws AccountNotFoundException {
+        for (BankAccount account : accounts) {
+            if (account.getAccountNumber() == accountNumber) {
+                return account;
+            }
+        }
+        throw new AccountNotFoundException("Рахунок #" + accountNumber + " не знайдено.");
+    }
+///перевод коштів
+    public void transferMoney(int fromAccountNumber, int toAccountNumber, double amount)
+            throws AccountNotFoundException, NegativeAmountException, InsufficientFundsException {
+        BankAccount fromAccount = findAccount(fromAccountNumber);
+        BankAccount toAccount = findAccount(toAccountNumber);
+
+        fromAccount.withdraw(amount);
+        toAccount.deposit(amount);
+    }
+//генерація номера - унікального номера акаунта
+    private int generateAccountNumber() {
+
+        return accounts.size() + 1;
+    }
+
+
 }
