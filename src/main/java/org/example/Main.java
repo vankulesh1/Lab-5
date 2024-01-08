4993de01be2d4c7b7342789516ce7cf49d2da5d9
NataliiaKulesh <ivankulesh0200@gmail.com>
2024-01-08 23:31:09 +0200

    перевірка проекту

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,27 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        // Створення об'єкту
+        Bank bank = new Bank();
 
+        // Створення рахунку
+        BankAccount firstaccount = bank.createAccount("Jonson", 1000);
+        BankAccount secondtaccount = bank.createAccount("Makron", 1500);
 
+        // Виведення інформації про рахунки
+        System.out.println(firstaccount.getAccountSummary());
+        System.out.println(secondtaccount.getAccountSummary());
+
+        // Переведення грошей з одного рахунку на другий
+        try {
+            bank.transferMoney(firstaccount.getAccountNumber(), secondtaccount.getAccountNumber(), 300);
+            System.out.println("Кошти успішно переведено.");
+        } catch (AccountNotFoundException | NegativeAmountException | InsufficientFundsException e) {
+            System.err.println("Відбулась помилка переведення коштів: " + e.getMessage());
+        }
+
+        // Виведення оновленої інформації про рахунки
+        System.out.println(firstaccount.getAccountSummary());
+        System.out.println(secondtaccount.getAccountSummary());
     }
 }
